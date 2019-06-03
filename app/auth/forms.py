9925dc3c22fde21c00fcda27e1565739dad58b57
canssidle <canssidlewairimu@gmail.com>
2019-06-03 17:15:48 +0300

    creating the login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,17 +14,17 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
-#     def validate_email(self,data_field):
-#         if User.query.filter_by(email =data_field.data).first():
-#             raise ValidationError('There is an account with that email')
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
 
-#     def validate_username(self,data_field):
-#         if User.query.filter_by(username = data_field.data).first():
-#             raise ValidationError('That username is taken')
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
 
 
-# class LoginForm(FlaskForm):
-#     email = StringField('Email', validators = [Required(), Email()])
-#     password = PasswordField('Password', validators = [Required()])
-#     remember = BooleanField('Remember Me ')
-#     submit = SubmitField('Log in ')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators = [Required(), Email()])
+    password = PasswordField('Password', validators = [Required()])
+    remember = BooleanField('Remember Me ')
+    submit = SubmitField('Log in ')
