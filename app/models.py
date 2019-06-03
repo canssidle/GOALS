e527835fe35226aa56be13c05e174e839c5ca326
canssidle <canssidlewairimu@gmail.com>
2019-06-03 16:54:33 +0300

    creating the user class

--- app/models.py
+++ app/models.py
@@ -7,19 +7,19 @@ from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
-#     return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
-# class User(UserMixin,db.Model):
-#     __tablename__= 'users'
-#     id = db.Column(db.Integer,  primary_key = True)
-#     username = db.Column(db.String(255))
-#     email = db.Column(db.String(255),unique = True, index =True)
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     pass_secure = db.Column(db.String(255))
-#     post = db.relationship('Blogpost',backref='user',lazy='dynamic')
-#     comments = db.relationship('Comment', backref = 'comment', lazy= "dynamic")
-#     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+class User(UserMixin,db.Model):
+    __tablename__= 'users'
+    id = db.Column(db.Integer,  primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    post = db.relationship('Blogpost',backref='user',lazy='dynamic')
+    comments = db.relationship('Comment', backref = 'comment', lazy= "dynamic")
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 #     @property
 #     def password(self):
 #         raise AttributeError('You cannot read the password attribute')
