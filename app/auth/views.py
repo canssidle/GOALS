e07e9a836699e0fdc2a8df47fcb8e6facb48e343
canssidle <canssidlewairimu@gmail.com>
2019-06-03 17:14:07 +0300

    creating the auth resiter route

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,19 +19,19 @@ def login():
     title = "perfect blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
-#         mail_message("Welcome to perfect blog","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to perfect blog","email/welcome_user",user.email,user=user)
 
-#         return redirect(url_for('auth.login'))
-#         title = "New Account"
-#     return render_template('auth/register.html',registration_form = form)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 # @auth.route('/register_admin',methods = ["GET","POST"])
 # @login_required
 # def register_admin():
