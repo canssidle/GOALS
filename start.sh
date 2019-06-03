b8a0f01ab48da25d4ce4c59d5052882c9f6b00ea
canssidle <canssidlewairimu@gmail.com>
2019-06-03 16:47:14 +0300

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-# export MAIL_USERNAME="canssidlewairimu@gmail.com"
-# export MAIL_PASSWORD="kitengela110"
-# export SECRET_KEY='canssy'
+export MAIL_USERNAME="canssidlewairimu@gmail.com"
+export MAIL_PASSWORD="kitengela110"
+export SECRET_KEY='canssy'
 
-# export DATABASE_URL='postgresql+psycopg2://canssidle:judycharles@localhost/myblog'
-# python3.6 manage.py server
+export DATABASE_URL='postgresql+psycopg2://canssidle:judycharles@localhost/myblog'
+python3.6 manage.py server
