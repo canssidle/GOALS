a4a407481c0181c81663b247b6974dfb079a14ad
canssidle <canssidlewairimu@gmail.com>
2019-06-03 16:50:50 +0300

    create a test for the posts

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,16 +1,16 @@
-# import unittest
-# from app.models import Blogpost
+import unittest
+from app.models import Blogpost
 
-# class PostTest(unittest.TestCase):
-#     '''
-#     Test Class to test the behaviour of the Post class
-#     '''
+class PostTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Post class
+    '''
 
-#     def setUp(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_post = Blogpost(id=1,title='technology',subtitle = 'smart phones',author='canssy',date_posted='05/4/2019',content='work made easier',user_id= 9)
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_post = Blogpost(id=1,title='technology',subtitle = 'smart phones',author='canssy',date_posted='05/4/2019',content='work made easier',user_id= 9)
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_post,Blogpost))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_post,Blogpost))
