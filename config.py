bae65655b03f3fc0fef328799745fb7309860553
canssidle <canssidlewairimu@gmail.com>
2019-06-03 16:52:35 +0300

    setting configurations in the prodconfig

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 # class TestConfig(Config):
 #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://canssidle:judycharles@localhost/myblog_test'
