59c4805e1aa73d938ebc9dc4951aa0f490c9ede3
canssidle <canssidlewairimu@gmail.com>
2019-06-03 17:15:23 +0300

    creating the registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,18 +1,18 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,PasswordField,BooleanField,SubmitField
-# from wtforms.validators import Required,Email,EqualTo
-# from ..models import User
-# from wtforms import ValidationError
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
 
 
 
-# class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm):
 
-#     email = StringField('Your Email Address',validators=[Required(),Email()])
-#     username = StringField('Enter your username',validators = [Required()])
-#     password = PasswordField('Password',validators = [Required(), EqualTo('confirm_password',message = 'Passwords must match')])
-#     confirm_password = PasswordField('Confirm Passwords',validators = [Required()])
-#     submit = SubmitField('Sign Up')
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('confirm_password',message = 'Passwords must match')])
+    confirm_password = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
 
 #     def validate_email(self,data_field):
 #         if User.query.filter_by(email =data_field.data).first():
