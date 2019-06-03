4968340e7e589dddc22dc4c59f6850df2adf01ca
canssidle <canssidlewairimu@gmail.com>
2019-06-03 16:47:52 +0300

    setting the app in development mode

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-# from app import create_app ,db
-# from flask_script import Manager,Server
-# from app.models import User,Role,Comment,Blogpost
-# from flask_migrate import Migrate , MigrateCommand
+from app import create_app ,db
+from flask_script import Manager,Server
+from app.models import User,Role,Comment,Blogpost
+from flask_migrate import Migrate , MigrateCommand
 
-# app = create_app('development')
+app = create_app('development')
 
 
 # manager = Manager(app)
